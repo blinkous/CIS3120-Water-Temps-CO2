13ce0aa7389c67854fb8b82fa94cf9e6bf627ef2
blinkous <saradhani1@gmail.com>
2019-05-16 21:44:40 -0400

    removed a file

--- water.py
+++
@@ -1,5 +0,0 @@
-import create_web_page as w
-
-filename = 'file:///Users/Sara/Documents/Python/Group_Project/web_version/python_page.html'
-
-w.createHtml("I called functions to create this page again", filename)
